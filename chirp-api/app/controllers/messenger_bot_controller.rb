75d9631eab73767116eb4703c37cab2ac10896e9
Harri Bell-Thomas <harribt@live.co.uk>
2017-04-08 18:41:55 +0100

    Refactored code

--- chirp-api/app/controllers/messenger_bot_controller.rb
+++ chirp-api/app/controllers/messenger_bot_controller.rb
@@ -106,173 +106,16 @@ class MessengerBotController < ActionController::Base
 
                 client = Wit.new(access_token: "FXLDTGT5HV5FGZX3VO2DEZXRH4B3K2NA", actions: actions)
                 rsp = client.converse(@current.fbid, text, @current.context)
-                sender.reply({ text: "#{rsp}" })
+                #sender.reply({ text: "#{rsp['msg']}" })
+                text_reply("#{rsp['msg']}", sender)
 
             end
         else
-            sender.reply({ text: "The event text is nil. Reply: #{event['message']['text']}" })
+            text_reply("The event text is nil. Reply: #{event['message']['text']}", sender)
+            #sender.reply({ text: "The event text is nil. Reply: #{event['message']['text']}" })
         end
 
-        #     sender.reply({
-        #     "attachment":{
-        #       "type":"image",
-        #       "payload":{
-        #         "url":"https://github.com/apple-touch-icon.png"
-        #       }
-        #     }
-        #   })
-        #
-        #   sender.reply({
-        #     "attachment":{
-        #       "type":"template",
-        #       "payload":{
-        #         "template_type":"button",
-        #         "text":"What do you want to do next?",
-        #         "buttons":[
-        #           {
-        #             "type":"web_url",
-        #             "url":"https://github.com/jun85664396/messenger-bot-rails",
-        #             "title":"Show Website"
-        #           },
-        #           {
-        #             "type":"postback",
-        #             "title":"Start Chatting",
-        #             "payload":"USER_DEFINED_PAYLOAD"
-        #           }
-        #         ]
-        #       }
-        #     }
-        #   })
-        #
-        #
-        #
-        #
-        #
-        #
-        # sender.reply({
-        #     "attachment":{
-        #       "type":"template",
-        #       "payload":{
-        #         "template_type":"generic",
-        #         "elements":[
-        #           {
-        #             "title":"Classic White T-Shirt",
-        #             "image_url":"http://petersapparel.parseapp.com/img/item100-thumb.png",
-        #             "subtitle":"Soft white cotton t-shirt is back in style",
-        #             "buttons":[
-        #               {
-        #                 "type":"web_url",
-        #                 "url":"https://petersapparel.parseapp.com/view_item?item_id=100",
-        #                 "title":"View Item"
-        #               },
-        #               {
-        #                 "type":"web_url",
-        #                 "url":"https://petersapparel.parseapp.com/buy_item?item_id=100",
-        #                 "title":"Buy Item"
-        #               },
-        #               {
-        #                 "type":"postback",
-        #                 "title":"Bookmark Item",
-        #                 "payload":"USER_DEFINED_PAYLOAD_FOR_ITEM100"
-        #               }
-        #             ]
-        #           },
-        #           {
-        #             "title":"Classic Grey T-Shirt",
-        #             "image_url":"http://petersapparel.parseapp.com/img/item101-thumb.png",
-        #             "subtitle":"Soft gray cotton t-shirt is back in style",
-        #             "buttons":[
-        #               {
-        #                 "type":"web_url",
-        #                 "url":"https://petersapparel.parseapp.com/view_item?item_id=101",
-        #                 "title":"View Item"
-        #               },
-        #               {
-        #                 "type":"web_url",
-        #                 "url":"https://petersapparel.parseapp.com/buy_item?item_id=101",
-        #                 "title":"Buy Item"
-        #               },
-        #               {
-        #                 "type":"postback",
-        #                 "title":"Bookmark Item",
-        #                 "payload":"USER_DEFINED_PAYLOAD_FOR_ITEM101"
-        #               }
-        #             ]
-        #           }
-        #         ]
-        #       }
-        #     }
-        #   })
-        #
-        #
-        #   sender.reply({
-        #       "attachment":{
-        #         "type":"template",
-        #         "payload":{
-        #           "template_type":"receipt",
-        #           "recipient_name":"Stephane Crozatier",
-        #           "order_number":"12345678902",
-        #           "currency":"USD",
-        #           "payment_method":"Visa 2345",
-        #           "order_url":"http://petersapparel.parseapp.com/order?order_id=123456",
-        #           "timestamp":"1428444852",
-        #           "elements":[
-        #             {
-        #               "title":"Classic White T-Shirt",
-        #               "subtitle":"100% Soft and Luxurious Cotton",
-        #               "quantity":2,
-        #               "price":50,
-        #               "currency":"USD",
-        #               "image_url":"http://petersapparel.parseapp.com/img/whiteshirt.png"
-        #             },
-        #             {
-        #               "title":"Classic Gray T-Shirt",
-        #               "subtitle":"100% Soft and Luxurious Cotton",
-        #               "quantity":1,
-        #               "price":25,
-        #               "currency":"USD",
-        #               "image_url":"http://petersapparel.parseapp.com/img/grayshirt.png"
-        #             }
-        #           ],
-        #           "address":{
-        #             "street_1":"1 Hacker Way",
-        #             "street_2":"",
-        #             "city":"Menlo Park",
-        #             "postal_code":"94025",
-        #             "state":"CA",
-        #             "country":"US"
-        #           },
-        #           "summary":{
-        #             "subtotal":75.00,
-        #             "shipping_cost":4.95,
-        #             "total_tax":6.19,
-        #             "total_cost":56.14
-        #           },
-        #           "adjustments":[
-        #             {
-        #               "name":"New Customer Discount",
-        #               "amount":20
-        #             },
-        #             {
-        #               "name":"$10 Off Coupon",
-        #               "amount":10
-        #             }
-        #           ]
-        #         }
-        #       }
-        #     })
 
-
-        # if !event['message']['text'].nil?
-        #     puts("Yay, got Wit.ai response: #{rsp}")
-        #
-        #
-        #     sender.reply({ text: "Yay, got Wit.ai response: #{rsp}" })
-        # else
-        #     sender.reply({ text: "Blank format" })
-        # end
-
-        #sender.reply({ text: "Reply: #{event['message']['text']}" })
     end
 
     def delivery(event, sender)
@@ -301,15 +144,15 @@ class MessengerBotController < ActionController::Base
         device_token = '12BF613DFAB7F8E763831FBFCFE3E339F518FB38CAD76ABADAB7D2BAC6F0D3E1'
 
         APNS.send_notification(
-        device_token,
-        :alert => 'Tap here to start a Chirp session.',
-        :badge => 1,
-        :sound => 'default',
-        :other => {
-            :chirp => {
-                :fbid => "#{id}"
+            device_token,
+            :alert => 'Tap here to start a Chirp session.',
+            :badge => 1,
+            :sound => 'default',
+            :other => {
+                :chirp => {
+                    :fbid => "#{id}"
+                }
             }
-        }
         )
     end
 
@@ -333,4 +176,18 @@ class MessengerBotController < ActionController::Base
         return nil if val.nil?
         return val.is_a?(Hash) ? val['value'] : val
     end
-end
+
+    def text_reply(txt, sender)
+        sender.reply({ text: txt })
+    end
+
+    def image_reply(url, sender)
+        sender.reply({
+            "attachment":{
+                "type":"image",
+                "payload":{
+                    "url": url
+                }
+            }
+        })
+    end
