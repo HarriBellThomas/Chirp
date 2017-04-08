97107586c5dce4172230a19df3417b3698c13d2f
Harri Bell-Thomas <harribt@live.co.uk>
2017-04-08 10:30:34 +0100

    Test wit integration

--- chirp-api/app/controllers/messenger_bot_controller.rb
+++ chirp-api/app/controllers/messenger_bot_controller.rb
@@ -1,7 +1,23 @@
 class MessengerBotController < ActionController::Base
   def message(event, sender)
     # profile = sender.get_profile(field) # default field [:locale, :timezone, :gender, :first_name, :last_name, :profile_pic]
-    sender.reply({ text: "Reply: #{event['message']['text']}" })
+
+    actions = {
+      send: -> (request, response) {
+        puts("sending... #{response['text']}")
+      },
+      my_action: -> (request) {
+        return request['context']
+      },
+    }
+
+    client = Wit.new(access_token: ENV["WIT_ACCESS_TOKEN"], actions: actions)
+
+    rsp = client.message(event['message']['text'])
+    puts("Yay, got Wit.ai response: #{rsp}")
+
+
+    sender.reply({ text: "Yay, got Wit.ai response: #{rsp}" })
   end
 
   def delivery(event, sender)
