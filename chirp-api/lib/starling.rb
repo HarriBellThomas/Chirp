b3c244c6ef2e2d978abc5494db2d515727a3bd2c
Oliver Hope <oliverdnhope@gmail.com>
2017-04-08 20:27:03 +0100

    Replace "User" with "Conversation".

--- chirp-api/lib/starling.rb
+++ chirp-api/lib/starling.rb
@@ -3,20 +3,20 @@ require 'json'
 
 class Starling
 	def self.balance(fbid)
-		u = User.find_by(:fbid => fbid)
+		u = Conversation.find_by(:fbid => fbid)
 		access_token = u.starling_access
 		response = RestClient.get('https://api-sandbox.starlingbank.com/api/v1/accounts/balance', headers={Accept: 'application/json', Authorization: 'Bearer '+access_token})
 		return response['availableToSpend']
 	end
 
 	def self.transfer(fbid, amount, contact_uuid)
-		u = User.find_by(:fbid => fbid)
+		u = Conversation.find_by(:fbid => fbid)
 		access_token = u.starling_access
 		#TODO: actually do transfer
 	end
 
 	def self.check_contact(fbid, contact_name)
-		u = User.find_by(:fbid => fbid)
+		u = Conversation.find_by(:fbid => fbid)
 		access_token = u.starling_access
 		response = RestClient.get('https://api-sandbox.starlingbank.com/api/v1/contacts', headers={Accept: 'application/json', Authorization: 'Bearer '+access_token})
 		contacts = JSON.parse(response.body)
@@ -32,7 +32,7 @@ class Starling
 	end
 
 	def self.spending(fbid, simple_date)
-		u = User.find_by(:fbid => fbid)
+		u = Conversation.find_by(:fbid => fbid)
 		access_token = u.starling_access
 		response = RestClient.get('https://api-sandbox.starlingbank.com/api/v1/transactions?'+simple_date, headers={Accept: 'application/json', Authorization: 'Bearer '+access_token})
 		return response
