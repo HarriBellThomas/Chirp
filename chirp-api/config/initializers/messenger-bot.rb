8201f61052d1c170c559cd4232b8f03fd748185d
Harri Bell-Thomas <harribt@live.co.uk>
2017-04-08 10:14:25 +0100

    Typo fix

--- chirp-api/config/initializers/messenger-bot.rb
+++ chirp-api/config/initializers/messenger-bot.rb
@@ -1,5 +1,5 @@
 Messenger::Bot.config do |config|
-  config.access_token = EAAF1aXwDCyUBAFjTAZAKpRvWNx4X6yMnAf3AzCVfU3xkwKGWA0QmRRfJHSxgMfHJ3NXQmrO543oV2ZA1p09fPUIfcP7xmgzAmhQwGKmValD7PImtBjZBnfFuxRrPCtJKw1rRHbWF3qnKiZC7NOJb2krjBMXs0TyV1vJS78cxCwZDZD
-  config.validation_token = 998be0c9-5d83-4adc-94b2-9705baffc6d6
-  config.secret_token = 13404c777cd943e238b0249de75d4c0e
+  config.access_token = "EAAF1aXwDCyUBAFjTAZAKpRvWNx4X6yMnAf3AzCVfU3xkwKGWA0QmRRfJHSxgMfHJ3NXQmrO543oV2ZA1p09fPUIfcP7xmgzAmhQwGKmValD7PImtBjZBnfFuxRrPCtJKw1rRHbWF3qnKiZC7NOJb2krjBMXs0TyV1vJS78cxCwZDZD"
+  config.validation_token = "998be0c9-5d83-4adc-94b2-9705baffc6d6"
+  config.secret_token = "13404c777cd943e238b0249de75d4c0e"
 end
