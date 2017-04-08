d0c12a968c1de9bc6615d46908978f64c3bd89ea
Harri Bell-Thomas <harribt@live.co.uk>
2017-04-08 18:14:28 +0100

    New db model

--- chirp-api/db/migrate/20170408170924_create_conversations.rb
+++ chirp-api/db/migrate/20170408170924_create_conversations.rb
@@ -1,7 +1,11 @@
 class CreateConversations < ActiveRecord::Migration
   def change
     create_table :conversations do |t|
-
+        t.string :uuid
+        t.string :fbid
+        t.string :starling_access
+        t.string :starling_refresh
+        t.text :context
       t.timestamps null: false
     end
   end
--- chirp-api/db/schema.rb
+++ chirp-api/db/schema.rb
@@ -11,6 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20170408170924) do
+
+  create_table "conversations", force: :cascade do |t|
+    t.string   "uuid"
+    t.string   "fbid"
+    t.string   "starling_access"
+    t.string   "starling_refresh"
+    t.text     "context"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+  end
 
 end
