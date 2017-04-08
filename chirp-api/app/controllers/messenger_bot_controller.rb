c5731dc8e530429d3cbf818388ec03623969a291
Harri Bell-Thomas <harribt@live.co.uk>
2017-04-09 00:00:05 +0100

    Moved first_entity_value over to wit int class

--- chirp-api/app/controllers/messenger_bot_controller.rb
+++ chirp-api/app/controllers/messenger_bot_controller.rb
@@ -91,11 +91,4 @@ class MessengerBotController < ActionController::Base
         send_test_push_notification(current.uuid, msg)
     end
 
-    def first_entity_value(entities, entity)
-        return nil unless entities.has_key? entity
-        val = entities[entity][0]['value']
-        return nil if val.nil?
-        return val.is_a?(Hash) ? val['value'] : val
-    end
-
 end
